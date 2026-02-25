e5041e39d5e7d88b1ee9c03870c5fa80de79765e
Auric Health Labs <contact@aurichealthlabs.com>
2026-02-25 08:37:38 -0800

    Add: Chapter 3 HIPAA PHI list

---
+++ pcc_ch_3_list_hipaa_phi.py
@@ -0,0 +1,16 @@
+# HIPAA Compliance Note: This program uses no PHI.
+# All examples are synthetic/educational.
+
+
+
+# HIPAA Safe Harbor de-identification of PHI 18 Identifiers
+# Note: This will most likely become a tuple and a module in some way
+
+phi = ['names', 'all geographic subdivisions smaller than state', 'all elements of dates except year', 'telephone numbers', 'fax numbers', 'electronic mail address', 'social security numbers', 'medical records numbers', 'health plan beneficiary numbers', 'account numbers', 'certificate and/or license numbers', 'vehicle identifiers and serial numbers', 'device identifiers and serial numbers', 'web universal resource locators (url)', 'internet protocol address numbers (ip)', 'biometric identifiers', 'full face photographic images and any comparable images', 'any other unique identifying number, characteristic, or code']
+# Source p. 97 hipaa-simplification-201303.pdf
+
+print()
+print(f'List index total: {len(phi)}')
+print(f'This list contains ' + str(len(phi)) + ' total identifiers to be de-identified.\n')
+
+# Repeat: this might eventually become a tuple to maintain order
