6b4c7d6b254de28872dc7e2a189e6e7572e3a660
Auric Health Labs <contact@aurichealthlabs.com>
2026-02-27 02:49:33 -0800

    Add: Chapter 4 HIPAA PHI loop as tuple instead of list

---
+++ pcc_ch_4_list_loop_hipaa_phi.py
@@ -0,0 +1,21 @@
+# DELETE THIS LINE BEFORE GIT PUSH = ONLY UPLOAD CH.3 FOR NOW
+# HIPAA Compliance Note: This program uses no PHI.
+# All examples are synthetic/educational.
+
+
+
+# HIPAA Safe Harbor de-identification of PHI 18 Identifiers
+# Changed to a tuple
+# Tuples use () instead of [] to surround the list.
+
+phi = ('names', 'all geographic subdivisions smaller than state', 'all elements of dates except year', 'telephone numbers', 'fax numbers', 'electronic mail address', 'social security numbers', 'medical records numbers', 'health plan beneficiary numbers', 'account numbers', 'certificate and/or license numbers', 'vehicle identifiers and serial numbers', 'device identifiers and serial numbers', 'web universal resource locators (url)', 'internet protocol address numbers (ip)', 'biometric identifiers', 'full face photographic images and any comparable images', 'any other unique identifying number, characteristic, or code')
+# Source p. 97 hipaa-simplification-201303.pdf
+
+print()
+print(f'List index total: {len(phi)}')
+print(f'This list contains ' + str(len(phi)) + ' total identifiers to be de-identified.\n')
+
+print('This list is now a tuple, i.e. immutable\n')
+for info in phi:
+    print(info)
+print()
